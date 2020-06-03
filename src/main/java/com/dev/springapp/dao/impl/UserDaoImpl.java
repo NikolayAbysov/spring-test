60d232f66c1ce7553db538256257632c43b2fb6e
NikolayAbysov <nikolay.abysov@ukr.net>
2020-06-03 11:05:48 +0300

    Issues fixed

--- src/main/java/com/dev/springapp/dao/impl/UserDaoImpl.java
+++ src/main/java/com/dev/springapp/dao/impl/UserDaoImpl.java
@@ -1,10 +1,13 @@
 package com.dev.springapp.dao.impl;
 
 import com.dev.springapp.dao.UserDao;
+import com.dev.springapp.exception.DataProcessingException;
 import com.dev.springapp.model.User;
 import java.util.List;
 import javax.persistence.TypedQuery;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -19,13 +22,34 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void add(User user) {
-        sessionFactory.openSession().save(user);
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Error while adding user. Stacktrace: ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory
-                .openSession().createQuery("from User", User.class);
-        return query.getResultList();
+        List<User> list;
+        try (Session session = sessionFactory.openSession()) {
+            TypedQuery<User> query = session
+                    .createQuery("from User", User.class);
+            list = query.getResultList();
+        }
+        return list;
     }
 }
